4db5e69db9f37003673cf18bb993aee3bbf0f648
Evelyne <evelyne.arits162@gmail.com>
2022-05-15 22:21:23 +0100

    Setting up the server/express/mongoDD

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Evelyne:Saphir162@cluster0.sfddy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
+
+module.exports = app;
