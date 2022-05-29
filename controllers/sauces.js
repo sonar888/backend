02a4d0cb1ffd95dc2b4c8a9539f5a5eba3bebdf8
Evelyne <evelyne.arits162@gmail.com>
2022-05-29 19:20:28 +0100

    Post call for likes and dislikes

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -8,11 +8,14 @@ exports.getAllSauces = (req, res, next) => {
 };
 
 exports.createSauce = (req, res, next) => {
-    const sauceObject = JSON.parse(req.body.sauce);
+  const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
     const sauce = new Sauce({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+        likes : 0,
+        dislikes : 0
+
         
     });
     sauce.save()
@@ -50,3 +53,144 @@ exports.modifySauce = (req, res, next) => {
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
   };
+
+  exports.likeSauce =  async (req, res, next) => {
+    try {
+      let sauce = await Sauce.findById(req.params.id)
+      let userId = req.body.userId
+      let like = req.body.like
+      console.log(sauce)
+     
+
+      if (!sauce.usersLiked.includes(userId) && !sauce.usersDisliked.includes(userId) ) {
+        switch (like) {
+          case 1 : console.log("like");
+          sauce.usersLiked.push(userId);
+          sauce.likes = sauce.usersLiked.length;
+          sauce.save();
+          return res.status(201).json(sauce);
+          break;
+
+          case -1 : console.log("dislike");
+          sauce.usersDisliked.push(userId);
+          sauce.dislikes = sauce.usersDisliked.length
+          sauce.save();
+          return res.status(201).json(sauce);
+          break;
+
+          case 0 : console.log("0");
+          return res.status(409 ).json({message: 'The request could not be completed due to a conflict with the current state of the resource'})
+        }
+
+      } else if (sauce.usersLiked.includes(userId)) {
+        switch (like) {
+          case 1 : console.log("1");
+          return res.status(409).json({message: 'The user already liked this sauce'})
+          break;
+
+          case -1 : console.log("-1");
+          return res.status(200).json({ message: 'The user cannot like and dislike the same sauce'});
+          break;
+
+          case 0 : console.log("remove like!");
+          const index = sauce.usersLiked.indexOf(userId)
+          sauce.usersLiked.splice(index, 1)
+          sauce.likes = sauce.usersLiked.length;
+
+          sauce.save()
+          return res.status(200).json(sauce);
+          break;
+  
+        }
+
+      } else if (sauce.usersDisliked.includes(userId)) {
+
+        switch (like) {
+          case 1 : console.log("1");
+          return res.status(409).json({message: 'The user cannot like and dislike the same sauce'})
+          break;
+
+          case -1 : console.log("-1");
+          return res.status(200).json({ message: 'The user already disliked this sauce'});
+          break;
+
+          case 0 : console.log("remove dislike!");
+          const index = sauce.usersDisliked.indexOf(userId)
+          sauce.usersDisliked.splice(index, 1)
+          sauce.dislikes = sauce.usersDisliked.length
+          sauce.save()
+          return res.status(200).json(sauce);
+          break;
+  
+        }
+
+        
+
+      }
+
+     
+  
+        console.log(userId, sauce.name)
+
+    }
+
+  
+    catch (err) {
+      console.log (err)
+    }
+    
+
+  }
+
+     
+
+   
+  //   Sauce.findById(req.params.id )
+  //   .then(() => {
+  //   const likeObject = JSON.parse(req.body.sauce);
+  // //   const like = new Sauce({
+  // //     ...likeObject
+      
+  // // });
+  //   // const userId = sauceObject.userId
+  //   // const like = sauceObject.like
+
+  //   console.log(likeObject)
+
+  //   })
+    
+    
+    // sauce.save()
+    //     .then(sauce => res.status(201).json(sauce))
+    //     .catch(error => res.status(400).json({ error }));
+
+    //await sauce findbyID
+    //récupérer le user id et like dans le body
+    // si user Id est présent dqns userliked; alors supprimer dans user like, sinon supprimer dans user dislike
+        //faire un recherhche dans tableaux et supprimer dans le tableaux dans lequel il existe
+    //mettre à jour la sauce
+    //  Sauce.findById(req.params.id)
+    // .then( sauce => {
+    //   if (like === '1') {
+    //     sauce.likes += 1 
+    //     sauce.usersLiked.push(userId)
+    //     console.log("He likes!")
+    //     res.status(200).json(usersLiked)
+    //   } else if ( like === '-1') {
+    //     sauce.dislikes += 1 
+    //     sauce.usersDisliked.push(userId)
+    //     console.log("!")
+    //     console.log("He dislikes!")
+    //     res.status(200).json(usersDisliked)
+    //   } else if (like ==='0') {
+    //     // 
+        // res.status(200).json({ message: sauceObject})
+      // }
+      
+
+
+
+
+    // })
+    // .catch(error => res.status(400).json({ error }))
+ 
--- routes/sauces.js
+++ routes/sauces.js
@@ -7,10 +7,13 @@ const multer = require('../middleware/multer-config');
 const saucesCtrl = require('../controllers/sauces');
 
 router.get('/', multer, saucesCtrl.getAllSauces); //auth
-router.post('/',auth, multer, saucesCtrl.createSauce); //, multer auth
 router.get('/:id', multer, saucesCtrl.getOneSauce);
 
+
+router.post('/',auth, multer, saucesCtrl.createSauce); //, multer auth
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+
+router.post('/:id/like', saucesCtrl.likeSauce)
 
 module.exports = router;
